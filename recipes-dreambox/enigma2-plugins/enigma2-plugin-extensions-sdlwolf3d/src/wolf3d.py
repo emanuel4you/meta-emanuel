e09da917daf82606f25e8c1cc6835df8e4fe3115
Emanuel Strobel <emanuel@ihad.tv>
2017-02-17 02:09:23 +0100

    enigma2 plugin sdlwolf3d: rename wrong file names

--- recipes-dreambox/enigma2-plugins/enigma2-plugin-extensions-sdlwolf3d/src/wolf3d.py
+++ recipes-dreambox/enigma2-plugins/enigma2-plugin-extensions-sdlwolf3d/src/wolf3d.py
@@ -1,19 +1,48 @@
 # -*- coding: utf-8 -*-
-from Plugins.Plugin import PluginDescriptor
+# code by emanuel@ihad.tv
 
-def main(session, **kwargs):
-	from wolf3d import Wolf3d
-	session.open(Wolf3d)
+from enigma import eConsoleAppContainer, fbClass, eRCInput
 
-def Plugins(**kwargs):
-	return [
-		PluginDescriptor(name=_("Wolf3d"), description=_("Wolf3d Gaming Engine for dreambox"),
-		   icon="g3icon_wolf3d.png",
-		   where = PluginDescriptor.WHERE_EXTENSIONSMENU,
-		   fnc=main),
-		PluginDescriptor(name=_("Wolf3d"), description=_("Wolf3d Gaming Engine for dreambox"),
-		   icon="plugin.png",
-		   where=PluginDescriptor.WHERE_PLUGINMENU,
-		   fnc=main)
-		]
+from Components.Language import language
+from Components.Sources.StaticText import StaticText
 
+from Screens.Screen import Screen
+from Screens.ServiceStopScreen import ServiceStopScreen
+
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+from Plugins.Extensions.GameBrowser.browser import GameSummary
+
+from skin import loadSkin
+
+loadSkin(resolveFilename(SCOPE_PLUGINS, "Extensions/GameBrowser/skin.xml"))
+
+#-----------------------------------------------------------------------------------
+
+class Wolf3d(Screen, ServiceStopScreen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		ServiceStopScreen.__init__(self)
+		self.skinName = ["Wolf3d"]
+		self["title"] = StaticText("Wolf3d")
+		self["lcdinfo"] = StaticText("")
+		self.__container=eConsoleAppContainer()
+		self.__appClosed_conn = self.__container.appClosed.connect(self.__runFinished)
+		self.stopService()
+		self.__runEmu()
+		
+	def __runEmu(self):
+		print "[Wolf3d] - __runEmu"
+		eRCInput.getInstance().lock()
+		fbClass.getInstance().lock()
+		com = "export LANG=" + language.getLanguage() + ".UTF-8;"
+		com += "/usr/bin/wolf3d-sdl-start;"
+		self.__container.execute(com)
+
+	def __runFinished(self,retval):
+		print "[Wolf3d] - __runFinished"
+		fbClass.getInstance().unlock()
+		eRCInput.getInstance().unlock()
+		self.close()
+		
+	def createSummary(self):
+		return GameSummary
